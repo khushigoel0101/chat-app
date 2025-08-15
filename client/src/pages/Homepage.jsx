1bed262b773cca83b98441927ecda88d66594938
khushigoel0101 <kgoel286@gmail.com>
2025-08-15 14:35:13 +0530

    chat section added with image upload option

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,25 +1,42 @@
-import React from 'react'
-import Sidebar from '../components/Sidebar'
-import ChatContainer from '../components/ChatContainer'
-import RightSidebar from '../components/RightSidebar'
+import React from 'react';
+import Sidebar from '../components/Sidebar';
+import ChatContainer from '../components/ChatContainer';
+import RightSidebar from '../components/RightSidebar';
 
 const Homepage = () => {
-       
-    const [selectedUser, setSelectedUser] = React.useState(null)
+  const [selectedUser, setSelectedUser] = React.useState(null);
 
   return (
-    <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
-        <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? 
-            'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'
-        }`}>
-    <div>
-        <Sidebar />
-        <ChatContainer />
-        <RightSidebar/>
-        </div>
-        </div>
-    </div>
-  )
-}
+    <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
+      <div
+        className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid grid-cols-1 relative ${
+          selectedUser
+            ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]'
+            : 'md:grid-cols-2'
+        }`}
+      >
+        
+        <Sidebar
+          selectedUser={selectedUser}
+          setSelectedUser={setSelectedUser}
+        />
 
-export default Homepage
+        
+        <ChatContainer
+          selectedUser={selectedUser}
+          setSelectedUser={setSelectedUser}
+        />
+
+        
+        {selectedUser && (
+          <RightSidebar
+            selectedUser={selectedUser}
+            setSelectedUser={setSelectedUser}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
